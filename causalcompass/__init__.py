ad3bcf92dc1d95c1d800baa1fb19da7618f7d9d8
huiyang-yi <huiyang_yi@163.com>
2026-04-20 17:45:46 +0800

    Sync package version from metadata

--- causalcompass/__init__.py
+++ causalcompass/__init__.py
@@ -1,4 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from . import algorithms
 from . import datasets
 
-__version__ = "0.1.0"
+try:
+    __version__ = version("causalcompass")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
